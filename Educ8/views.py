4d3b751e149c44f0e4861e8ac228e8f792cea533
Cameron McDonald <2570726m@student.gla.ac.uk>
2022-03-06 13:50:52 +0000

    cookies fix

--- Educ8/views.py
+++ Educ8/views.py
@@ -54,7 +54,7 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-def visitor_cookie_handler(request, response):
+def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
