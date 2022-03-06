f54aec4ea50e8adea9067338b6d1ae0a378d4c72
David Whelan <2569877w@student.gla.ac.uk>
2022-03-06 12:34:58 +0000

    adsfa

--- Educ8/urls.py
+++ Educ8/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('restricted/', views.restricted, name='restricted'),
-    path('logout/', views.user_logout, name='logout'),
+    path('logout/', views.logout, name='logout'),
     
 ]
