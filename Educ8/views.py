68beed528c6babc2fd63b4ae05ffc3379cfa7637
David Whelan <2569877w@student.gla.ac.uk>
2022-03-06 11:54:42 +0000

    added some imports and view headers

--- Educ8/views.py
+++ Educ8/views.py
@@ -3,3 +3,17 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Educ8 says aight!")
+
+def home(request):
+    pass
+
+def show_course(request, category_name_slug):
+    pass
+
+def add_course(request):
+    pass
+
+def add_flashcard(request):
+    pass
+
+
