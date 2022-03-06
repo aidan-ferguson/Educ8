5dd121e187417e44e18b999d5375433a1b98fe84
Michael McVey <2558835m@student.gla.ac.uk>
2022-03-06 14:44:02 +0000

    admin changes

--- Educ8/admin.py
+++ Educ8/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from Educ8.models import Teacher, Student
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name')
+
 admin.site.register(Teacher)
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
