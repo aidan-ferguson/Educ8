2a9a43e53571525a04c64185d7bf6ea152751ce9
Cameron McDonald <2570726m@student.gla.ac.uk>
2022-03-06 14:39:50 +0000

    secure passwords

--- django_group_project/settings.py
+++ django_group_project/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': { 'min_length': 8, },
+        'OPTIONS': { 'min_length': 9, },
 
     },
     {
@@ -107,9 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-]
+    ]
 
 
 # Internationalization
