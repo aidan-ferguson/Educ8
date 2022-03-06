44eeb91adfa7345f65b16eecf205966cafa10fba
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-03-06 15:46:30 +0000

    readded course and flashcard tables

--- Educ8/models.py
+++ Educ8/models.py
@@ -46,42 +46,42 @@ class Student(models.Model):
         return self.user.username
 
 
-# class Course(models.Model):
-#     """Course class: This is a model for our Course table in our database.
+class Course(models.Model):
+    """Course class: This is a model for our Course table in our database.
 
-#     Attributes:
-#         courseName (str): This is the name of the Course.
-#         createdBy (str): This is the username of the Teacher that created the Course.
-#         students (list of strings): This is a list of the usernames of the Students that have been enrolled on the Course.
+    Attributes:
+        courseName (str): This is the name of the Course.
+        createdBy (str): This is the username of the Teacher that created the Course.
+        students (list of strings): This is a list of the usernames of the Students that have been enrolled on the Course.
 
-#     Methods:
-#     __str__ : Returns the name of the Course. This makes debugging easier.
-#     """
-#     courseName = models.CharField(max_length=256, primary_key=True)
-#     createdBy = models.ForeignKey(Teacher, on_delete = models.CASCADE)
-#     students = models.ManyToManyField(Student, blank=True)
+    Methods:
+    __str__ : Returns the name of the Course. This makes debugging easier.
+    """
+    courseName = models.CharField(max_length=256, primary_key=True)
+    createdBy = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    students = models.ManyToManyField(Student, blank=True)
 
-#     def __str__(self):
-#         return self.courseName
+    def __str__(self):
+        return self.courseName
     
-# class Flashcard(models.Model):
-#     """Flashcard class: This is a model for our Flashcard table in our database.
+class Flashcard(models.Model):
+    """Flashcard class: This is a model for our Flashcard table in our database.
 
-#     Attributes:
-#         title (str): This is the title of the Flashcard.
-#         question (str): This is the question of the Flashcard.
-#         answer (str): This is the answer of the Flashcard.
-#         createdBy (str): This is the username of the Student who created the Flashcard.
-#         Course (str): This is the Course of the Student, for which this Flashcard belongs to.
+    Attributes:
+        title (str): This is the title of the Flashcard.
+        question (str): This is the question of the Flashcard.
+        answer (str): This is the answer of the Flashcard.
+        createdBy (str): This is the username of the Student who created the Flashcard.
+        Course (str): This is the Course of the Student, for which this Flashcard belongs to.
 
-#     Methods:
-#     __str__ : Returns the title of the Flashcard. This makes debugging easier.
-#     """
-#     title = models.CharField(max_length=32, primary_key=True, unique=True)
-#     question = models.CharField(max_length=256)
-#     answer = models.CharField(max_length=256)
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-#     createdBy = models.ForeignKey(Student, on_delete=models.CASCADE)
+    Methods:
+    __str__ : Returns the title of the Flashcard. This makes debugging easier.
+    """
+    title = models.CharField(max_length=32, primary_key=True, unique=True)
+    question = models.CharField(max_length=256)
+    answer = models.CharField(max_length=256)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    createdBy = models.ForeignKey(Student, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
