7400e9fe67553d66fa28f865df07d24861a3977a
Timothy Wang <2556936w@student.gla.ac.uk>
2022-03-06 12:55:05 +0000

    DUMMY: Initial flashcard section design, add dummy dummy index to templa

--- dummy_site_tim/assets/scripts/scripts_course.js
+++ dummy_site_tim/assets/scripts/scripts_course.js
@@ -2,4 +2,14 @@
 
 $(document).ready(function(){
 
+  $(".flashcard_tile").each(function() {
+
+    // Background color generator
+    var red = Math.floor(Math.random() * 100) + 155;
+    var green = Math.floor(Math.random() * 100) + 155;
+    var blue = Math.floor(Math.random() * 100) + 155;
+    $(this).css("background-color", `rgb(${red}, ${green}, ${blue})`);
+
+  });
+
 });
