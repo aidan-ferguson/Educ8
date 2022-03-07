c1f252c37fb6038b8e4c4ef0c29c5ab7f5a96d41
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-03-07 21:49:12 +0000

    removed adminstudent

--- Educ8/admin.py
+++ Educ8/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 from Educ8.models import Teacher, Student, Course, Flashcard, CourseFile
 
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name')
-
 # Register following models with the admin interface
 # TO_REGISTER = [Teacher, Student, Course, CourseFile, Flashcard]
 # list(map(lambda x: admin.site.register(x), TO_REGISTER))
