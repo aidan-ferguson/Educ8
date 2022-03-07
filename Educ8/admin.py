be5d9ab75069d37e140717e218a131e33352879a
Michael McVey <2558835m@student.gla.ac.uk>
2022-03-07 20:37:03 +0000

    Admin UI changes

--- Educ8/admin.py
+++ Educ8/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from Educ8.models import Teacher, Student, Course, Flashcard
-
-# Register following models with the admin interface
-TO_REGISTER = [Teacher, Student, Course, Flashcard]
-list(map(lambda x: admin.site.register(x), TO_REGISTER))
+from Educ8.models import Teacher, Student, Course, Flashcard, CourseFile
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name')
+
+# Register following models with the admin interface
+TO_REGISTER = [Teacher, Student, StudentAdmin, Course, CourseFile, Flashcard]
+list(map(lambda x: admin.site.register(x), TO_REGISTER))
+
