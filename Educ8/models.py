94193b7902662425e219b81df43291f00a214c92
Dom Jina <2524785j@student.gla.ac.uk>
2022-03-10 15:29:31 +0000

    fixed min lengths

--- Educ8/models.py
+++ Educ8/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 """
@@ -24,7 +25,7 @@ class Teacher(models.Model):
     Methods:
     __str__ : Returns the username of the Teacher. This makes debugging easier.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, min_length=MINIMUM_LENGTH, max_length=MAX_LENGTH_USERNAME)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, validators=[MinLengthValidator(MINIMUM_LENGTH)], max_length=MAX_LENGTH_USERNAME)
 
     def __str__(self):
         return self.user.username
@@ -41,7 +42,7 @@ class Student(models.Model):
     Methods:
     __str__ : Returns the username of the Student. This makes debugging easier.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, min_length=MINIMUM_LENGTH, max_length=MAX_LENGTH_USERNAME)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, validators=[MinLengthValidator(MINIMUM_LENGTH)], max_length=MAX_LENGTH_USERNAME)
 
     def __str__(self):
         return self.user.username
@@ -58,7 +59,7 @@ class Course(models.Model):
     Methods:
     __str__ : Returns the name of the Course. This makes debugging easier.
     """
-    courseName = models.CharField(max_length=256, primary_key=True)
+    courseName = models.CharField(max_length=256, primary_key=True, validators=[MinLengthValidator(4)])
     createdBy = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
     students = models.ManyToManyField(Student, blank=True)
 
@@ -78,9 +79,9 @@ class Flashcard(models.Model):
     Methods:
     __str__ : Returns the title of the Flashcard. This makes debugging easier.
     """
-    title = models.CharField(max_length=128)
-    question = models.CharField(max_length=1024)
-    answer = models.CharField(max_length=1024)
+    title = models.CharField(max_length=128,validators=[MinLengthValidator(1)])
+    question = models.CharField(max_length=1024, validators=[MinLengthValidator(1)])
+    answer = models.CharField(max_length=1024, validators=[MinLengthValidator(1)])
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     createdBy = models.ForeignKey(Student, on_delete=models.CASCADE)
 
