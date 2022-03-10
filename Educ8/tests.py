85f65139ce2cbe698affd2b2e29bddef7153b4a4
David Whelan <2569877w@student.gla.ac.uk>
2022-03-10 15:59:40 +0000

    started test for slug creation

--- Educ8/tests.py
+++ Educ8/tests.py
@@ -1,2 +1,10 @@
 from django.test import TestCase
+from Educ8.models import Course
 
+def CourseMethodTests(TestCase):
+    def test_slug_line_creation(self):
+        
+        course = Course(courseName='Example Course')
+        course.save()
+        
+        self.assertEqual(course.slug, 'example-course')
