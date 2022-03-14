30f13febeb4d81af6af91a666519fe226e9b477f
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-03-14 21:38:17 +0000

    batch fixed

--- reset_db.bat
+++ reset_db.bat
@@ -3,7 +3,7 @@ echo Y | del /f .\db.sqlite3
 echo Y | del /f  .\Educ8\migrations
 python .\manage.py makemigrations Educ8
 python .\manage.py migrate
-python .\manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')"
+python .\manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin', 'admin@example.com', 'password')"
 echo:
 echo:
 echo Username : admin
