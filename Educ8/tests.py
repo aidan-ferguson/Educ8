3c132b7b1d22ff94ad160173fe552bdd701c014a
Cameron McDonald <2570726m@student.gla.ac.uk>
2022-03-14 20:25:14 +0000

    IndexViewTests

--- Educ8/tests.py
+++ Educ8/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from Educ8.models import Course
+from django.urls import reverse
+
 
 class CourseMethodTests(TestCase):
     def test_slug_line_creation(self):
@@ -8,8 +10,12 @@ class CourseMethodTests(TestCase):
         course.save()
         
         self.assertEqual(course.slug, 'example-course')
-        
-    def test_created_by_someone(self):
-        
-        course = Course(createdBy=)
-        
+
+class IndexViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('Educ8:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome to Educ8')
+        self.assertContains(response, 'The Greatest Online Learning Platform (GOLP)')
+        self.assertContains(response, 'Login')
+        self.assertContains(response, 'Register')
