5d9d82ca94c71ce60c7b44578af8fe0f5de6d794
Timothy Wang <2556936w@student.gla.ac.uk>
2022-03-14 21:49:26 +0000

    pain

--- dummy_site_tim/assets/scripts/scripts_flashcard.js
+++
@@ -1,8 +0,0 @@
-/* WAD2 Team Project "Educ8" | Flashcard Page Visual Scripts */
-
-$(document).ready(function(){
-
-  // Generate background colour
-  timGenerator($(".card_panel").find("h2").text(), $(".card_panel"));
-
-});
