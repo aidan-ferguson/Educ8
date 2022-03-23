dceed42dd4039b6ceebd149b915ff47da5f937b7
Timothy Wang <2556936w@student.gla.ac.uk>
2022-03-23 23:24:07 +0000

    Misc. index changes

--- static/assets/scripts/scripts_index.js
+++ static/assets/scripts/scripts_index.js
@@ -1,11 +1,7 @@
 /* WAD2 Team Project "Educ8" | "My Courses" Page Visual Scripts */
 
 $(document).ready(function(){
-
   $("#activate_cat").click(function() {
-
     $(".cat_use").slideToggle();
-
   });
-
 });
