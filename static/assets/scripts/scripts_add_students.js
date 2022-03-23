1cbd7df657d1f6ef2226ff973dee6dfa806090b9
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-03-23 20:11:07 +0000

    current students list now updates

--- static/assets/scripts/scripts_add_students.js
+++ static/assets/scripts/scripts_add_students.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
 
     $("#add_student").click(function() {
   
-      let student_to_add = $("#selection_field").find(":selected").attr("username");
-      $.get('add_student/', {'student': student_to_add},
+      let student_to_add = $("#selection_field").find(":selected");
+      $.get('add_student/', {'student': student_to_add.attr("username")},
         function(data, status) {
-            if(status !== "success"){
+            if(status === "success"){
+                $("#current_students").append("<li>" + student_to_add.text() + "</li>")
+                student_to_add.remove()
+            } else {
                 $("ajax_error").html(data);
             }
         });
