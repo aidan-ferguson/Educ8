d06e7069f2114c2d516faef13b5f5bd12c001c77
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-03-24 16:55:03 +0000

    Can now delete students that have been added without refreshing

--- static/assets/scripts/scripts_students.js
+++ static/assets/scripts/scripts_students.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
         let add_student_url = $("#selection_field").attr("add_student_url");
         $.ajax({
             url: add_student_url,
-            data: {'student': student_to_add.attr("username")},
+            data: {'student': student_to_add.attr("value")},
             success: function(data, status) {
-                $("#current_students").append("<li>" + student_to_add.text() + "</li>")
+                let bin_button = '<i class="fa fa-trash-can remove_student" student_username="' + student_to_add.attr("value") +'"></i>'
+                let new_entry = $("#current_students").append("<li><p>" + student_to_add.text() + bin_button + "</p></li>")
+                new_entry.find("p").find("i").click(remove_student);
                 student_to_add.remove()
             },
             error: function(xhr, textStatus, errorText){
@@ -19,20 +21,28 @@ $(document).ready(function(){
         });  
     });
 
-    $(".remove_student").click(function() {
+    function remove_student(button) {
         let clicked_element = $(this);
-        let remove_student_url = $(this).attr("remove_student_url");
+        let remove_student_url = $(this).parent().parent().parent().attr("remove_student_url");
+        console.log("click")
         $.ajax({
             url: remove_student_url,
             data: {'student': clicked_element.attr("student_username")},
             success: function(data, status) {
-                clicked_element.parent().remove();
+                $("#selection_field").append($('<option>',{
+                    value: clicked_element.attr("student_username"),
+                    text: clicked_element.parent().text(),
+                }));
+                clicked_element.parent().parent().remove();
             },
             error: function(xhr, textStatus, errorText){
                 if(xhr.status == 500){
                     $("#ajax_error").html(xhr.responseText);
                 }
             }
-        });  
-    });
+        });
+    }
+
+    $(".remove_student").click(remove_student);  
+    
 });
